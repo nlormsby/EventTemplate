f91d8f75897c43b1908610038d7dc322dab01020
Eric Zheng <114521575+erizheng@users.noreply.github.com>
2024-10-21 23:06:49 -0700

    first try

--- event-app/src/App.tsx
+++ event-app/src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
 import { Redirect } from "./components/Redirect";
+import { mainPage } from './views/mainPage';
 
-function App() {
-  let redirectURL = "https://github.com/nlormsby/EventTemplate"
+// function App() {
+//   let redirectURL = "https://github.com/nlormsby/EventTemplate"
   
-  return (
-    <div className="AppContainer">
-      <Redirect url={redirectURL} text='Click Here' />
-    </div>
-  );
-}
+//   return (
+//     <div className="AppContainer">
+//       <Redirect url={redirectURL} text='Click Here' />
+//       <mainPage />
+//     </div>
+//   );
+// }
+
+const App = () => {
+  // HINT: Wrap the MyBudgetTracker component with AppContextProvider
+    return <mainPage />;
+
+  
+};
 
 export default App;
---
+++ event-app/src/views/mainPage.tsx
@@ -0,0 +1,8 @@
+
+export const mainPage = () => {
+    return (
+        <div className="container">
+            
+        </div>
+    );
+};
