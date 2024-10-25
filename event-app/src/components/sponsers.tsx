207177371536e4af2908c1241a6fa93dc897d6db
Eric Zheng <114521575+erizheng@users.noreply.github.com>
2024-10-24 23:47:47 -0700

    Sponsers pictures added

--- event-app/src/components/sponsers.tsx
+++ event-app/src/components/sponsers.tsx
@@ -4,7 +4,13 @@ export const Sponser = () => {
         <div id="spons">
              <h1>Sponsers</h1>
              <hr></hr>
-            {/* images */}
+             <img className="sPic" src="images\HCF_LLNL.png"/>
+             <img className="sPic" src="images\HCF_HRL.png"/>
+             <img className="sPic" src="images\HCF_EXTREME.png"/>
+             <img className="sPic" src="images\HCF_DWE.png"/>
+             <img className="sPic" src="images\HCF_servicenow.png"/>
+             <img className="sPic" src="images\HCF_PSemi.png"/>
+             <img className="sPic" src="images\HCF_intel.png"/>
         </div>
     );
 };
